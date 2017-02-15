5890d36d692996958bc92e955c87d76ff0c352b0
jefferson oliveira <cardbeatt@gmail.com>
2017-02-15 18:00:35 -0200

    heroku changes

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -9,10 +9,11 @@ export default class Landing extends React.Component {
         <h1> <a target="_blank" href="https://www.freecodecamp.com/challenges/timestamp-microservice">Free Code Camp</a></h1>
         <h2>API project : Timestamp microservice </h2>
         <h3>Example usage:</h3>
-        <p>https://floating-depths-12924.herokuapp.com/21323123123</p>
-        <p>https://floating-depths-12924.herokuapp.com/August%2021,%201993</p>
-        <p>https://floating-depths-12924.herokuapp.com/TestingRandomTextToGetNullJSON</p>
+        <p>https://sheltered-meadow-55777.herokuapp.com/21323123123</p>
+        <p>https://sheltered-meadow-55777.herokuapp.com/August%2021,%201993</p>
+        <p>https://sheltered-meadow-55777.herokuapp.com/TestingRandomTextToGetNullJSON</p>
       </div>
     )
   }
 }
+https://sheltered-meadow-55777.herokuapp.com
