5ac1a00986fb7d6f14ac84ebcd6fc36766d0daa8
jefferson oliveira <cardbeatt@gmail.com>
2017-02-15 13:20:09 -0200

    timestamp functionality

--- server.js
+++ server.js
@@ -12,14 +12,34 @@ app.get('/', function(req,res) {
 
 //timestamps
 app.get('/:query',function(req, res) {
-  var time = new Date()
-  var day = time.getDate();
-  var month = time.getMonth();
-  var year = time.getFullYear();
-  res.send(month+ " " + day + ", " + year)
+  let query = req.params.query;
+  res.json(makeTimestamp(query));
 })
 
+function makeTimestamp(timestamp) {
+  var result {
+    unix: null,
+    natural: null
+  }
+}
 
+let date;
+if(!isNaN(ParseInt(timestamp))) {
+  date = new Date(parseInt(timestamp)); 
+} else {
+  date = new Date(timestamp);
+}
+
+if(!isNaN(date.getTime())) {
+  result.unix = date.getTime();
+  result.natural = getNatural(date);
+}
+
+function getNaturalDate(date){
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September', 'October', 'November', 'December'];
+
+  return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+}
 // port
 app.listen(PORT, function() {
   console.log('Server running at ' + PORT)
