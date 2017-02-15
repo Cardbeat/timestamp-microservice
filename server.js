d9a5c7b313f927bc0470e37c1421d9d4d8d578cf
jefferson oliveira <cardbeatt@gmail.com>
2017-02-15 13:09:45 -0200

    server finished

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
-
+const PORT = process.env.PORT  || 3000
 
 //routes
 
+
+//root
 app.get('/', function(req,res) {
   res.send('Hello Worl!')
 })
 
+//timestamps
+app.get('/:query',function(req, res) {
+  var time = new Date()
+  var day = time.getDate();
+  var month = time.getMonth();
+  var year = time.getFullYear();
+  res.send(month+ " " + day + ", " + year)
+})
 
-app.listen(3000, function() {
-  console.log('Server running at localhost:3000')
+
+// port
+app.listen(PORT, function() {
+  console.log('Server running at ' + PORT)
 });
